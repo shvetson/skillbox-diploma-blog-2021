f2b5d680ab935bc3efc273599028121a7d098561
macbook <shvetson@gmail.com>
2021-09-15 23:43:53 +0300

    commit 2021/09/15

--- src/main/java/ru/shvets/blog/repositories/CaptchaRepository.java
+++ src/main/java/ru/shvets/blog/repositories/CaptchaRepository.java
@@ -1,9 +1,20 @@
 package ru.shvets.blog.repositories;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.shvets.blog.models.CaptchaCode;
+
+import java.util.List;
 
 @Repository
 public interface CaptchaRepository extends JpaRepository<CaptchaCode, Long> {
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "delete from captcha_codes where time_to_sec(timediff(now(), time)) / 3600 > 4")
+    void deleteAllTimeGreaterThanHour();
 }
--- src/main/java/ru/shvets/blog/services/CaptchaService.java
+++ src/main/java/ru/shvets/blog/services/CaptchaService.java
@@ -1,34 +1,52 @@
 package ru.shvets.blog.services;
 
+import com.github.cage.Cage;
 import com.github.cage.GCage;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.shvets.blog.dto.CaptchaDto;
+import ru.shvets.blog.models.CaptchaCode;
 import ru.shvets.blog.repositories.CaptchaRepository;
 
-import javax.servlet.http.HttpSession;
-import java.awt.image.BufferedImage;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
 public class CaptchaService {
     private final CaptchaRepository captchaRepository;
 
-    public CaptchaDto getCaptcha(){
+    public CaptchaDto getCaptcha() {
         final String PREFIX = "data:image/png;base64, ";
 
         CaptchaDto captchaDto = new CaptchaDto();
-        GCage gCage = new GCage();
+        Cage gCage = new GCage();
 
         String token = gCage.getTokenGenerator().next();
-        String image = Base64.getEncoder().encodeToString(token.getBytes());
+        String image = Base64.getEncoder().encodeToString(gCage.draw(token));
 
         captchaDto.setSecret(token);
         captchaDto.setImage(PREFIX.concat(image));
 
+        CaptchaCode captchaCode = new CaptchaCode();
+        captchaCode.setTime(new Date());
+        captchaCode.setSecretCode(getSecretCode(10));
+        captchaCode.setCode(token);
+        captchaRepository.save(captchaCode);
+
+        captchaRepository.deleteAllTimeGreaterThanHour();
         return captchaDto;
+    }
+
+    public String getSecretCode(int length) {
+        Random random = new Random();
+        String alphabet = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        StringBuilder code = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            code.append(alphabet.charAt(random.nextInt(alphabet.length()-1) + 1));
+        }
+        return code.toString();
     }
 }
